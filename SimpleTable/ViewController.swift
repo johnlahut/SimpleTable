e7558d7902c20c9d7b7c62723b9426f9187fe494
lahut13 <lahut13@gmail.com>
2016-09-22 01:44:15 -0400

    Simple textbook walkthrough that demonstrates use of TableView in iOS

--- SimpleTable/ViewController.swift
+++ SimpleTable/ViewController.swift
@@ -8,7 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var restaurantNames = ["Cafe Deadend",
+                           "Homei",
+                           "Teakha",
+                           "Cafe Loisl",
+                           "Petite Oyster",
+                           "For Kee Restaurant",
+                           "Po's Atelier",
+                           "Bourke Street Bakery",
+                           "Haigh's Chocolate",
+                           "Palomino Espresso",
+                           "Upstate",
+                           "Traif",
+                           "Graham Avenue Meats And Deli",
+                           "Waffle & Wolf",
+                           "Five Leaves",
+                           "Cafe Lore",
+                           "Confessional",
+                           "Barrafina",
+                           "Donostia",
+                           "Royal Oak",
+                           "CASK Pub and Kitchen"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +40,34 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return restaurantNames.count;
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+        
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        var imgname = cell.textLabel?.text
+        
+        imgname! = imgname!.stringByReplacingOccurrencesOfString(" ",
+            withString: "").stringByReplacingOccurrencesOfString("&",
+            withString: "").stringByReplacingOccurrencesOfString("'", withString: "").localizedLowercaseString
+        
+        cell.imageView?.image = UIImage(named: imgname!)
+        
+        return cell
+        
     }
 
 
